c3c894987bfa353c4141876b5ebcf509e7431919
Kevin Hubert <iamkevinhubert@gmail.come>
2019-03-20 16:24:14 -0500

    reverting adding styles

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Header extends React.Component {
-  render() {
-    return <div className="l-header" />;
-  }
-}
-
-export default Header;
--- src/components/Sidebar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Sidebar extends React.Component {
-  render() {
-    return <div className="l-sidebar">Here's the sidebar yo</div>;
-  }
-}
-
-export default Sidebar;
