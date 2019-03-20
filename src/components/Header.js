19f5f647f732d4ec68ecfe61dea97123f3c83176
Kevin Hubert <iamkevinhubert@gmail.come>
2019-03-20 16:18:25 -0500

    adding change

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return (
-      <div className="l-header">
-        <h1>This is the header component</h1>
-      </div>
-    );
+    return <div className="l-header" />;
   }
 }
 
